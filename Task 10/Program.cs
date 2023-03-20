789efc28bd70743b4ec54ac7dbd9ce0b023fc9b6
Grigoriy <kutsaevg@yandex.ru>
2023-03-20 19:37:34 +0300

    Выполнил домашнее задание ко второму семинару, задачи 10, 13, 15.

---
+++ Task 10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число ");
+int digit = int.Parse(Console.ReadLine());
+
+if (digit > 99 & digit < 1000)
+{
+   int digit_1 = digit % 100;
+   int digit_2 = digit_1 / 10;
+   Console.WriteLine($"Вторая цифра: {digit_2}");
+}
+
+else 
+{
+    Console.WriteLine("Число не является трехзначным");
+}
---
+++ Task 13/Program.cs
@@ -0,0 +1,18 @@
+﻿/* // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("Введите трехзначное число: ");
+double number = double.Parse(Console.ReadLine());
+
+if (number > 3)
+{
+    number = double.Parse(Math.Pow(10, number - 3));
+    Console.WriteLine("Третья цифра: ");
+}
+else
+{
+    if (number <= 2) ;
+    Console.WriteLine("Третьей цифры нет.!");
+}
